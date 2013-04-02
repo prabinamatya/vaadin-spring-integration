f5b5a4425a96341cbe7a7f997dcf30fd9d63321f
prabinamatya <pnamatya@gmail.com>
2013-04-01 22:09:15 -0500

    added environmentsetup scripts

---
+++ documents/environmentScript.bat
@@ -0,0 +1,14 @@
+SETX GRADLE_HOME="C:\Development\Softwares\gradle-1.4"
+SETX CXF_HOME="C:\Development\Softwares\apache-cxf-2.7.2"
+SETX M2_HOME = "C:\Development\Softwares\Applications\apache-maven-3.0.5"
+SETX JAVA_HOME = "C:\Program Files (x86)\Java\jdk1.7.0_13"
+SETX MAVEN_OPTS = "-Xms256m -Xmx512m"
+SETX GRADLE_HOME ="C:\Development\Softwares\Applications\gradle-1.4"
+SETX CATALINA_HOME ="C:\Development\Softwares\apache-tomcat-7.0.35"
+CATALINA_OPTS="-Xdebug -Xrunjdwp:transport=dt_socket,address=7878,server=y,suspend=n"
+JPDA_OPTS="-agentlib:jdwp=transport=dt_socket,address=7878,server=y,suspend=n"
+
+
+SETX path="C:\Users\prabin\algs4\bin;C:\Users\prabin\algs4\java\bin;
+%JAVA_HOME%\bin;
+C:\Program Files (x86)\Sikuli X\libs;%M2_HOME%\bin;%GRADLE_HOME%\bin;%CATALINA_HOME%\bin"
